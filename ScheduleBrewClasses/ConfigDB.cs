5097a15244d8ed5bc9362ba52b72640ec31b3bd4
Mara's PC Laptop <williamsmc@my.lanecc.edu>
2022-11-21 12:10:30 -0800

    Add project files.

---
+++ ScheduleBrewClasses/ConfigDB.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleBrewClasses
+{
+    public class ConfigDB
+    {
+        public static string GetMySqlConnectionString()
+        {
+            string folder = System.AppContext.BaseDirectory;
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(folder)
+                    .AddJsonFile("mySqlSettings.json", optional: true, reloadOnChange: true);
+
+            string connectionString = builder.Build().GetConnectionString("mySql");
+
+            return connectionString;
+        }
+    }
+}
