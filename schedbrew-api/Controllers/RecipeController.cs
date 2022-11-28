ad1e31f99260b5124f88ab2680133f926f06a1a5
Mara's Mac <williamsmc@my.lanecc.edu>
2022-11-28 15:09:45 -0800

    first try on c/p statecontroller for recipe controller

--- schedbrew-api/Controllers/RecipeController.cs
+++ schedbrew-api/Controllers/RecipeController.cs
@@ -1,13 +1,137 @@
 ﻿using System;
-namespace schedbrew_api.Controllers
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScheduleBrewClasses.Models;
+
+namespace MMABooksEF_RESTfulAPI.Controllers
 {
-	public class RecipeController
-	{
-		public RecipeController()
-		{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly ScheduleBrewContext _context;
 
-			//started with empty class
-		}
-	}
+        public RecipeController(ScheduleBrewContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Recipe
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipes()
+        {
+            if (_context.Recipes == null)
+            {
+                return NotFound();
+            }
+            return await _context.Recipes.ToListAsync();
+        }
+
+        // GET: api/Recipe/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(int id)
+        {
+            if (_context.Recipes == null)
+            {
+                return NotFound();
+            }
+            var state = await _context.Recipes.FindAsync(id);
+
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return state;
+        }
+
+        // PUT: api/Recipe/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipe(int id, Recipe recipe)
+        {
+            if (id != recipe.RecipeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Recipe
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        {
+            if (_context.Recipes == null)
+            {
+                return Problem("Entity set 'ScheduleBrewContext.Recipes'  is null.");
+            }
+            _context.Recipes.Add(recipe);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (RecipeExists(recipe.RecipeId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetRecipe", new { id = recipe.RecipeId }, recipe);
+        }
+
+        // DELETE: api/Recipe/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipe(int id)
+        {
+            if (_context.Recipes == null)
+            {
+                return NotFound();
+            }
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _context.Recipes.Remove(recipe);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RecipeExists(int id)
+        {
+            return (_context.Recipes?.Any(e => e.RecipeId == id)).GetValueOrDefault();
+        }
+    }
 }
-
