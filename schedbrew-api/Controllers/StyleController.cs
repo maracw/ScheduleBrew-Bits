55d2d5a8661304fe9cdef775edee374f1a5d31dc
Mara's Mac <williamsmc@my.lanecc.edu>
2022-11-28 16:42:05 -0800

    add efcore relational as own package. works. batch, style get all and get by id work in swagger

---
+++ schedbrew-api/Controllers/StyleController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScheduleBrewClasses.Models;
+
+namespace schedbrew_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatchController : ControllerBase
+    {
+        private readonly ScheduleBrewContext _context;
+
+        public BatchController(ScheduleBrewContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Batch
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Batch>>> GetBatches()
+        {
+            if (_context.Recipes == null)
+            {
+                return NotFound();
+            }
+            return await _context.Batches.ToListAsync();
+        }
+        
+        // GET: api/Batch/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Batch>> GetBatch(int id)
+        {
+            if (_context.Batches == null)
+            {
+                return NotFound();
+            }
+            var batch = await _context.Batches.FindAsync(id);
+
+            if (batch == null)
+            {
+                return NotFound();
+            }
+
+            return batch;
+        }
+        /*
+        // PUT: api/Recipe/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipe(int id, Recipe recipe)
+        {
+            if (id != recipe.RecipeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Recipe
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        {
+            if (_context.Recipes == null)
+            {
+                return Problem("Entity set 'ScheduleBrewContext.Recipes'  is null.");
+            }
+            _context.Recipes.Add(recipe);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (RecipeExists(recipe.RecipeId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetRecipe", new { id = recipe.RecipeId }, recipe);
+        }
+
+        // DELETE: api/Recipe/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipe(int id)
+        {
+            if (_context.Recipes == null)
+            {
+                return NotFound();
+            }
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _context.Recipes.Remove(recipe);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        */
+        private bool BatchExists(int id)
+        {
+            return (_context.Batches?.Any(e => e.BatchId == id)).GetValueOrDefault();
+        }
+    }
+}
